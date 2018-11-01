514a45025ae9f1cf917565a64c08bc1bfdc7af11
hassa357 <hassa357@morris.umn.edu>
2018-11-01 03:02:39 -0500

    Add new methiods. Keep adding to the first getExchangeRate.

--- src/xrate/ExchangeRateReader.java
+++ src/xrate/ExchangeRateReader.java
@@ -3,6 +3,8 @@ package xrate;
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Properties;
+
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
@@ -10,9 +12,11 @@ import com.google.gson.JsonParser;
 /**
  * Provide access to basic currency exchange rate services.
  *
- * @author hunter jubair
+ * @author Hunter Jubair
  */
 public class ExchangeRateReader {
+
+
   String Url;
   String accessKey;
 
@@ -27,7 +31,10 @@ public class ExchangeRateReader {
      * @param baseURL
      *            the base URL for requests
      */
-    public ExchangeRateReader(String baseURL) {
+
+
+    public ExchangeRateReader(String baseURL) throws IOException {
+
         // TODO Your code here
         /*
          * DON'T DO MUCH HERE!
@@ -35,16 +42,37 @@ public class ExchangeRateReader {
          * the two methods below. All you need to do here is store the
          * provided `Url` in a field so it will be accessible later.
          */
-         this.Url = baseURL;
+
+        this.Url = baseURL;
+
+        readAccessKeys();
+
+    }
+
+
+    private void readAccessKeys() throws IOException {
+
+        Properties properties = new Properties();
+        FileInputStream input = null;
+
         try {
-            this.accessKey = new BufferedReader(new FileReader("src/accessKey")).readLine();
-        } catch (FileNotFoundException e) {
-            System.out.println("Make sure the file exists");
-            System.out.println(e);
-        } catch (IOException e) {
-            System.out.println("Something went wrong");
-            System.out.println(e);
+
+            input = new FileInputStream("etc/access_keys.properties");
+
         }
+
+        catch (FileNotFoundException exception) {
+
+            System.err.println("Couldn't open etc/access_keys.properties; have you renamed the sample file?");
+
+            throw(exception);
+
+        }
+
+        properties.load(input);
+
+        accessKey = properties.getProperty("fixer_io");
+
     }
 
     /**
@@ -62,11 +90,27 @@ public class ExchangeRateReader {
      * @return the desired exchange rate
      * @throws IOException
      */
+
+
+    // The getRate method
+
+    public float getRate(JsonObject ratesInfo, String currency){
+        return ratesInfo.getAsJsonObject("rates").get(currency).getAsFloat();
+    }
+
+
     public float getExchangeRate(String currencyCode, int year, int month, int day) throws IOException {
         // TODO Your code here
-        URL finalURL = createURL(year, month, day);
+
+        String modMonth = addZero(month);
+        String modDay = addZero(day);
+
+        String newURL = Url + year + "-" + modMonth + "-" + modDay + "?access_key=" + accessKey;
+
+        URL finalURL = new URL(newURL);
 
         InputStream inputStream = finalURL.openStream();
+
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
 
         JsonParser parser = new JsonParser();
@@ -77,6 +121,7 @@ public class ExchangeRateReader {
 
         return parsedExchangeRate;
     }
+
 
     /**
      * Get the exchange rate of the first specified currency against the second
@@ -96,7 +141,25 @@ public class ExchangeRateReader {
      * @throws IOException
      */
 
-    //write code to change single digit dates to 2 digit dates
+
+    // Write code to change single digit dates to 2 digit dates
+
+    public String addZero(int dateOrMonth) {
+
+        if (dateOrMonth < 10) {
+
+            return "0" + String.valueOf(dateOrMonth);
+
+        }
+
+        else {
+
+            return String.valueOf(dateOrMonth);
+
+        }
+
+    }
+
 
     public float getExchangeRate(
     String fromCurrency, String toCurrency,
